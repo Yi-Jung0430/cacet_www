4aeee03e0b7fad9a5831c6d1025c0062124c59c0
Yi-Jung0430 <yijunglin79013@gmail.com>
2022-11-18 17:34:32 +0800

    製作 header (未完成)

--- home.php
+++ home.php
@@ -4,6 +4,17 @@
     <!-- 導航欄 開始 -->
     <?php include("inc_navbar.php"); ?>
     <!-- 導航欄 結束 -->
+
+    <!-- header 開始 -->
+    <point class="container-fluid">
+      <div class="bg__line"></div>
+      <div class="main">
+        <p>123456</p>
+        <a><img src="storage/002.jpg"><span>我要投</span></a>
+        <a><img src="storage/003.jpg"><span>我要投</span></a>
+      </div>
+    </point>
+    <!-- Header 結束 -->
   </div>
 </div>
 
