f7608f5d99fbf2f912dafed64ec87a7ffd48a5cb
Yi-Jung0430 <yijunglin79013@gmail.com>
2022-12-05 11:07:45 +0800

    新增每頁模板

---
+++ seminar_manual.php
@@ -0,0 +1,18 @@
+<!-- 會議投稿說明 頁面 -->
+<?php include("inc_head.php"); ?>
+<div class="home bg-gray02">
+  <div class="home__bg01">
+    <!-- 導航欄 開始 -->
+    <?php include("inc_navbar.php"); ?>
+    <!-- 導航欄 結束 -->
+
+
+
+    
+
+  </div>
+</div>
+<?php include("inc_footer.php"); ?>
+<script>
+  
+</script>
---
+++ template.php
@@ -0,0 +1,18 @@
+<!-- 每頁模板 複製用 -->
+<?php include("inc_head.php"); ?>
+<div class="home bg-gray02">
+    <div class="home__bg01">
+        <!-- 導航欄 開始 -->
+        <?php include("inc_navbar.php"); ?>
+        <!-- 導航欄 結束 -->
+
+
+
+
+
+    </div>
+</div>
+<?php include("inc_footer.php"); ?>
+<script>
+
+</script>
