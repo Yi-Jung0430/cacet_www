be1d2ffc6acd96ab8e046592ec400e7aa25964f2
Yi-Jung0430 <yijunglin79013@gmail.com>
2023-02-21 11:30:39 +0800

    新增 404 頁面

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php include("inc_head.php"); ?>
+<div class="forget bg-gray02">
+    <div class="forget__bg01">
+        <!-- 內容 開始 -->
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-6 mx-auto bg-white rounded-3 shadow">
+                    <div class="p-lg-5 p-md-3 p-2">
+                        <h1 class="text-primary text-center">
+                            <i class="bi bi-emoji-dizzy me-2"></i>404 Error
+                        </h1>
+                        <h2 class="text-secondary text-center">
+                            抱歉 ! 找不到該頁面
+                        </h2>
+
+                        <div class="text-center mt-4">
+                            <a href="home_noLogin.php" class="btn btn-outline-third">前往首頁</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 內容 結束 -->
+</div>
